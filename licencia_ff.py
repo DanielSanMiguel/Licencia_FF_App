822cedc0faa826ed5f08f3d9f1ac185f9fea2e28
DanielSanMiguel <146175551+DanielSanMiguel@users.noreply.github.com>
2024-09-06 17:49:15 +0200

    Add files via upload

--- licencia_ff.py
+++ licencia_ff.py
@@ -24,37 +24,55 @@ if contrasena == contrasena_correcta:
         # Entrada para la URL
         url = " https://flyfut.olocip.com/licenses/create"
         
+        # Datos del usuario
+        nombre = st.text_input("Nombre y Apellidos", "")
+        club = st.text_input("Club", "")
+        puesto = st.text_input("Puesto/Cargo", "")
+        email = st.text_input("E-mail", "")
+        
         # Campos de entrada para el cuerpo de la solicitud
         
-        nombre = st.text_input("Nombre", "")
+        nombre_licencia = st.text_input("Nombre licencia (Ejemplo: Juan1)", "")
         
+        # Datos y credenciales AT
+        api_key = st.secrets['at_token']
+        base_id = 'appFezarrh9fv6WrS'
+        table_name = 'List_licencias'
+        url_at = 'https://api.airtable.com/v0/appFezarrh9fv6WrS/List_licencias'
+        data_at = {"records": [{"fields": {
+            'Nombre':nombre, 'Club':club, 'Puesto':puesto, 'Email':email}}]}
+        headers_at = {"Authorization" : f"Bearer {api_key}",  "Content-Type" : 'application/json' }
         # Botón para enviar la solicitud
-        if st.button("Enviar solicitud"):
-            # Cuerpo de la solicitud en formato JSON
-            data = {
-                'name': nombre
-            }
-        
-            # Encabezados (headers)
-            headers = {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'admin_name': 'flyfut',
-                'admin_password': admin_password
-                
-            }
-            json.dumps(data)
-            # Realizar la solicitud POST
-            try:
-                response = requests.post(url, data=data, headers=headers)
-        
-                # Mostrar los resultados
-                st.subheader("Resultado de la solicitud:")
-                st.code(f"Código de estado: {response.status_code}")
-                
-                # Mostrar la respuesta en formato JSON si es posible
+        if nombre and club and puesto and email and nombre_licencia:
+            if st.button("Solicitar 7 días de prueba"):
+                # Cuerpo de la solicitud en formato JSON
+                data = {
+                    'name': nombre_licencia
+                }
+            
+                # Encabezados (headers)
+                headers = {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'admin_name': 'flyfut',
+                    'admin_password': admin_password
+                    
+                }
+                json.dumps(data)
+                # Realizar la solicitud POST
                 try:
-                    st.json(response.json())
-                except json.decoder.JSONDecodeError:
-                    st.write(response.text)
-            except requests.exceptions.RequestException as e:
-                st.error(f"Error al enviar la solicitud: {e}")
+                    response_at = requests.post(url_at, data=data_at, headers=headers)
+                    response = requests.post(url, data=data, headers=headers)
+            
+                    # Mostrar los resultados
+                    st.subheader("Resultado de la solicitud:")
+                    st.code(f"Código de estado: {response.status_code}")
+                    
+                    # Mostrar la respuesta en formato JSON si es posible
+                    try:
+                        st.write(response.json())
+                    except json.decoder.JSONDecodeError:
+                        st.write(response.text)
+                except requests.exceptions.RequestException as e:
+                    st.error(f"Error al enviar la solicitud: {e}")
+            else:
+                st.warning("Por favor, rellena todos los campos para habilitar el botón de envío.")
