4eedca0d7c697a2603d3b6d071d7b64e82590cd5
DanielSanMiguel <146175551+DanielSanMiguel@users.noreply.github.com>
2024-09-09 16:54:46 +0200

    Add files via upload

--- licencia_ff.py
+++ licencia_ff.py
@@ -30,14 +30,6 @@ custom_css = """
 <style>
 div[data-baseweb="input"] {
     background-color: #c7c7c7;
-    border-top-color: #c7c7c7;
-    border-bottom-color: #c7c7c7;
-    border-right-color: #c7c7c7;
-    border-left-color: #c7c7c7;
-    border-top-style: groove;
-    border-bottom-style: groove;
-    border-right-style: groove;
-    border-left-style: groove;
     }
 div[data-baseweb="base-input"] input {
     background-color: #c7c7c7;  /* Fondo color  */
