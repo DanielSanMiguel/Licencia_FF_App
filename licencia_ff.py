2c3966f04855d28a7cb521f2cabc4d2eb358dbb1
DanielSanMiguel <146175551+DanielSanMiguel@users.noreply.github.com>
2024-09-09 16:26:44 +0200

    Add files via upload

--- licencia_ff.py
+++ licencia_ff.py
@@ -38,8 +38,8 @@ div[data-baseweb="input"] input {
     }
 
 div[data-baseweb="input"] {
-    margin: 0px;
-    border: 0px solid #FF6347;
+    margin: 0px 0px 0px;
+    border: none;
     }
 
 </style>
@@ -59,7 +59,7 @@ mkContainer = """
 <style>
 
 div[class="st-emotion-cache-1rsyhoq e1nzilvr5"] p {
-    margin: 0px;
+    margin: 0px 0px 0px;
     border: none;
     background: radial-gradient(circle, #fe603a, #fefb3a, #fe603a);
     color: black;
