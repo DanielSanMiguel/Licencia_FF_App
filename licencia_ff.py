2fe551960d8c7b0ff5ff708dc046cf574ecd7b1e
DanielSanMiguel <146175551+DanielSanMiguel@users.noreply.github.com>
2024-09-11 09:57:28 +0200

    Add files via upload

--- licencia_ff.py
+++ licencia_ff.py
@@ -71,7 +71,7 @@ div[data-testid="stAlertContainer"] {
 div[class="st-emotion-cache-1rsyhoq e1nzilvr5"] p {
     
     border: none;
-    background: linear-gradient(to left, #c2c1c0, #dcb06e, #de9e3f);
+    background: linear-gradient(to left, #c2c1c0, #dcb06e, #de9e3f, #dfc195, #dfaf66);
     border: 1px solid #0f0f0f;  /* Borde color  */
     border-radius: 10px;        /* Bordes redondeados */
     color: black;
