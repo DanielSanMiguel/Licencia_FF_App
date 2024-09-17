0f16ad681b7e54a64d213b4e5ace9056844781d6
DanielSanMiguel <146175551+DanielSanMiguel@users.noreply.github.com>
2024-09-17 19:39:31 +0200

    Add files via upload

--- licencia_ff.py
+++ licencia_ff.py
@@ -115,18 +115,17 @@ st.markdown(button_css, unsafe_allow_html=True)
 st.markdown(mkContainer, unsafe_allow_html=True)
 
 url_imagen_github_1 = "https://raw.githubusercontent.com/DanielSanMiguel/Licencia_FF_App/main/foto_ff.jpg"
-
-
+admin_password = st.secrets['admin_password']
+# Entrada para la URL
+url = " https://flyfut.olocip.com/licenses/create"
 
 with st.container():
-    admin_password = st.secrets['admin_password']
-    
     # Título de la aplicación
     st.title("Licencia App Fly-Fut")
     st.header('Rellene los campos para enviar el formulario')
     st.image(url_imagen_github_1, use_column_width='auto')
-    # Entrada para la URL
-    url = " https://flyfut.olocip.com/licenses/create"
+
+
     
     # Datos del usuario
     nombre = st.text_input("Nombre y Apellidos", "")
