c1ee8434eb5638e176737a4d72660626c034bfe9
DanielSanMiguel <146175551+DanielSanMiguel@users.noreply.github.com>
2024-09-17 19:46:04 +0200

    Add files via upload

--- licencia_ff.py
+++ licencia_ff.py
@@ -114,7 +114,7 @@ url = " https://flyfut.olocip.com/licenses/create"
 with st.container():
     # Título de la aplicación
     st.title("Licencia App Fly-Fut")
-    st.header('Rellene los campos para enviar el formulario')
+    st.subheader('Rellene los campos para enviar el formulario')
     st.image(url_imagen_github_1, use_column_width='auto')
 
 
